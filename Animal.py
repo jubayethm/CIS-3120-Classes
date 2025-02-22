840b6cc24e4b3bb2517ef3af6f9534a5324d02e0
jubayethm <jubayethm@gmail.com>
2025-02-21 21:35:07 -0500

    Added 5 new methods to Animal class and tested them

--- Animal.py
+++ Animal.py
@@ -6,3 +6,17 @@ class Animal:
     def talk(self):
         print("hi")
 
+    def eat(self, food):
+        print(f"{self.__name} is eating {food}")
+
+    def sleep(self, hours):
+        print(f"{self.__name} sleeps for {hours} hours")
+
+    def play(self, toy):
+        print(f"{self.__name} is playing with a {toy}")
+
+    def describe(self):
+        print(f"{self.__name} is a wonderful animal!")
+
+    def sound(self, noise="default sound"):
+        print(f"{self.__name} makes a {noise} sound")
